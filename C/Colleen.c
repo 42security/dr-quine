833354b39e1971385ca2af9a5f7cba980fe1c2f7
Luca Incerpi <lincerpi@e3r9p8.42roma.it>
2022-01-18 18:50:32 +0100

    Colleen.c

---
+++ C/Colleen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// comment
+void colleen() {}
+
+int main()
+{
+	colleen();
+	// comment
+	char s[]="#include <stdio.h>%1$c// comment%1$cvoid colleen() {}%1$c%1$cint main()%1$c{%1$c%2$ccolleen();%1$c%2$c// comment%1$c%2$cchar s[]=%3$c%4$s%3$c;%1$c%2$cprintf(s, 10, 9, 34, s);%1$c}";
+	printf(s, 10, 9, 34, s);
+}
