50d82373cb67539e690faac5169cf614afc841f1
Luca Incerpi <lincerpi@e3r10p4.42roma.it>
2022-01-22 16:27:42 +0100

    Colleen.py

--- Python/Colleen.py
+++ Python/Colleen.py
@@ -2,14 +2,20 @@
 def colleen(s):
 	print(s.format(s = s, c = chr(34)))
 
-if __name__ == '__main__':
+def main():
 	# comment
 	s = """# comment
 def colleen(s):
 	print(s.format(s = s, c = chr(34)))
 
-if __name__ == '__main__':
+def main():
 	# comment
 	s = {c}{c}{c}{s}{c}{c}{c}
-	colleen(s)"""
 	colleen(s)
+
+if __name__ == '__main__':
+	main()"""
+	colleen(s)
+
+if __name__ == '__main__':
+	main()
